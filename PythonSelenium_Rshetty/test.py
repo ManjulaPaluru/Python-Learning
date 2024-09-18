37b30a4cf1760ca9265d15fc365258b791b119b1
Manjula Paluru <manjula.paluru@gmail.com>
2024-09-18 11:14:48 -0700

    commiting rahul shetty python selenium classes files

---
+++ PythonSelenium_Rshetty/test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.implicitly_wait(10)
+driver.find_element(By.NAME," username").send_keys("Admin")
+driver.find_element(By.NAME,"password").send_keys("admin123")
+driver.find_element((By.XPATH,"//button[@type='submit']")).click()
