36956a8e41c59cc72158d9de9a7f856862e7ce34
Manjula Paluru <manjula.paluru@gmail.com>
2024-09-18 11:48:08 -0700

    pushing general practice programs to remote repro

---
+++ practice_genera_lprograms/practice.py
@@ -0,0 +1,40 @@
+# a = [3,4,2,6,8]
+# for i in a:
+#     print(i)
+# print(type(i))
+#
+# a1= [1,2,4,6,3,8]
+# for i in range(0,a1.__len__(),1):
+#     print(a1[i])
+
+ # for i in a1:
+ #     print(i)
+
+# slicing
+# syntax  list/tuple[startingindex:endindex:step]
+# end index is always -1
+# a=[100,200,300,400,500,600,700,800]
+# print(a[2::3])
+# print(a[-1])
+# print(a[-2])
+# print(a[0:a.__len__():1])
+# print(a[a.__len__()::-1])
+
+# append() and extend method
+# a=[1,2,3,4,5]
+# b=[3,4,6]
+#
+# # a.append(b)
+# # print(a)
+# # a.extend(b)
+# # print(a)
+# c= ['a','b']
+# # a.append(c)
+# # print(a)
+# a.extend(c)
+# print(a)
+
+
+# print even or odd no
+num = 110
+print("even no") if num % 2 == 0 else print("odd no")
